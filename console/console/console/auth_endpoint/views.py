5c7dfdda60e55c8b2fd1ee6121cb6e7101c31392
Sunny Xu <43919034+therealsunnyxu@users.noreply.github.com>
2025-05-26 15:39:50 -0700

    Revert "Revert "added logic and tests for resetting email and passwords""
    This reverts commit b6aa11b67d191605d15bc230f2cc3835f0760592.
    
     Please enter the commit message for your changes. Lines starting
     with '' will be ignored, and an empty message aborts the commit.
    
     On branch main
     Your branch is ahead of 'origin/main' by 4 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   .vscode/settings.json
    	new file:   console/console/console/auth_endpoint/migrations/0004_alter_forgotpasswordrequest_uuid_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0005_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0006_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0007_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0008_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0009_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0010_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	new file:   console/console/console/auth_endpoint/migrations/0011_alter_forgotpasswordchangerequest_expires_at_and_more.py
    	modified:   console/console/console/auth_endpoint/models.py
    	modified:   console/console/console/auth_endpoint/views.py
    	modified:   console/console/console/settings.py
    	modified:   console/console/console/templates/emails/reset_code_email.html.j2
    	new file:   console/console/console/tests/__init__.py
    	new file:   console/console/console/tests/conftest.py
    	new file:   console/console/console/tests/helper_functions.py
    	deleted:    console/console/console/tests/test_auth_endpoint.py
    	new file:   console/console/console/tests/test_change_email.py
    	new file:   console/console/console/tests/test_change_password.py
    	new file:   console/console/console/tests/test_forgot_password.py
    	new file:   console/console/console/tests/test_login_logout.py
    	modified:   console/console/console/urls.py
    	modified:   console/console/jwt_auth/backends.py
    	modified:   console/console/jwt_auth/tests/test_jwt_backend.py
    	modified:   console/console/pytest.ini
    	new file:   console/console/test_log.txt
    	modified:   console/requirements.txt

--- console/console/console/auth_endpoint/views.py
+++ console/console/console/auth_endpoint/views.py
@@ -1,9 +1,10 @@
 import secrets
 import sys
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AnonymousUser
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.middleware.csrf import get_token
@@ -47,6 +48,31 @@ def logout_handler(request: HttpRequest):
         del request.session["refresh_token"]
     if request.session.get("access_token"):
         del request.session["access_token"]
+
+    # Stop email and password change requests by making them all expire
+    # This means the user has to make the requests again when they log back in
+    # just in case someone somehow makes unauthorized API calls to reset emails and passwords
+    # using something like Postman or cURL
+    try:
+        if hasattr(request, "user") and request.user.id is not None:
+            user = request.user
+            password_requests = models.PasswordChangeRequest.objects.filter(user=user)
+            email_requests = models.EmailChangeRequest.objects.filter(user=user)
+            if password_requests and hasattr(password_requests, "count"):
+                for obj in password_requests:
+                    obj.expires_at = timedelta(seconds=0)
+                    obj.email_sent_at = datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)
+
+                models.PasswordChangeRequest.objects.bulk_update(password_requests, ["expires_at", "email_sent_at"])
+            if email_requests and hasattr(email_requests, "count"):
+                for obj in email_requests:
+                    obj.expires_at = timedelta(seconds=0)
+                    obj.email_sent_at = datetime(1970, 1, 1, 0, 0, tzinfo=timezone.utc)
+
+                models.EmailChangeRequest.objects.bulk_update(email_requests, ["expires_at", "email_sent_at"])
+    except Exception as e:
+        print("Could not nullify reset requests because", e)
+        pass
 
     # Flush out everything in the session just in case deleting didn't work
     request.session.flush()
@@ -95,6 +121,13 @@ def is_access_token_valid(request: HttpRequest):
 # Reset request handlers #
 
 def send_code_email(user: AbstractBaseUser, field_name: str, reset_code: int | str):
+    """Sends a reset email to a user
+
+    Args:
+        user (AbstractBaseUser): An existing user
+        field_name (str): The field that needs to be reset (email, password, etc.)
+        reset_code (int | str): An n-digit code
+    """
     user.email_user(
         f"Pet Cam: {field_name} Reset Code", 
         render_to_string("emails/reset_code_email.html.j2", 
@@ -129,7 +162,7 @@ def forgot_password_request_handler(request: HttpRequest):
 
         # unique session so that only the device that requested the reset is the one that fulfills it
         request_uuid = forgot_password_request.uuid
-        res.set_cookie(key="reset_pw_uuid", value=request_uuid, httponly=True, samesite="Lax",expires=datetime.now() + timedelta(days=1))
+        res.set_cookie(key="reset_pw_uuid", value=request_uuid, httponly=True, samesite="Lax",expires=datetime.now(timezone.utc) + timedelta(days=1))
 
     # Send a 200 regardless of whether or not the email actually exists in the system
     # to prevent brute force email scanning
@@ -151,7 +184,15 @@ def change_password_request_handler(request: HttpRequest):
     if not is_access_token_valid(request):
         return HttpResponse(content="Invalid or no access token", status=401)
 
+    if len(str(user.email)) < 1:
+        return HttpResponse(content="Unable to change password because you have no email. Please ask an administrator", status=401)
     new_password = form.cleaned_data.get("new_password")
+    confirm = form.cleaned_data.get("confirm_new_password")
+    if new_password != confirm:
+        return HttpResponse(content="Confirm new password must match new password", status=400)
+    is_new_same_as_old = user.check_password(new_password)
+    if is_new_same_as_old:
+        return HttpResponse(content="New password cannot be same as old password", status=400)
     reset_code = generate_reset_code()
     field_name = "Password"
     models.PasswordChangeRequest.objects.create(user=user, code=reset_code, new_password=new_password)
@@ -168,13 +209,18 @@ def change_email_request_handler(request: HttpRequest):
 
     # Don't allow user to submit change request if they're not logged in
     user: AbstractBaseUser = request.user
-    if not user:
+    if type(user) is AnonymousUser:
+        return HttpResponse("User must be logged in", status=401)
+    if user.id is None:
         return HttpResponse("Current session is invalid", status=400)
     
     if not is_access_token_valid(request):
         return HttpResponse(content="Invalid or no access token", status=401)
     
+    old_email = user.email
     new_email = form.cleaned_data.get("new_email")
+    if old_email == new_email:
+        return HttpResponse(content="New email is identical to old email", status=400)
     reset_code = generate_reset_code()
     field_name = "Email"
     models.EmailChangeRequest.objects.create(user=user, code=reset_code, new_email=new_email)
@@ -208,17 +254,17 @@ def forgot_password_code_handler(request: HttpRequest):
     if forgot_password_request.is_fulfilled():
         return HttpResponse(content="Code already used", status=400)
     
-    forgot_password_request.request_fulfilled_at = datetime.now()
+    forgot_password_request.request_fulfilled_at = datetime.now(timezone.utc)
     # make a part two to allow the user to reset their password given that their original request is now valid
     # they still have their reset password cookie
-    models.ForgotPasswordChangeRequest.objects.create(original_request=forgot_password_request)
+    models.ForgotPasswordChangeRequest.objects.create(original_request=forgot_password_request,expires_at=datetime.now(timezone.utc) + timedelta(days=1))
     return HttpResponse(content="Code accepted", status=200)
 
 @method(["POST"])
 def forgot_password_change_handler(request: HttpRequest):
     form = forms.ForgotPasswordChangeForm(request.POST)
 
-    if form.cleaned_data.get("cancelled") is not None:
+    if form.data.get("cancelled") is not None:
         res = HttpResponse(content="Successfully cancelled request", status=200)
         res.set_cookie("reset_pw_uuid", "", expires="0", httponly=True)
         return res
@@ -256,9 +302,9 @@ def forgot_password_change_handler(request: HttpRequest):
     
     user.set_password(new_password)
     user.save()
-    child_request.request_fulfilled_at = datetime.now()
+    child_request.request_fulfilled_at = datetime.now(timezone.utc)
     
-    return HttpResponse(status=418)
+    return HttpResponse(status=200)
 
 @method(["POST"])
 def change_email_code_handler(request: HttpRequest):
@@ -287,7 +333,7 @@ def change_email_code_handler(request: HttpRequest):
     
     user.email = change_email_request.new_email
     user.save()
-    change_email_request.request_fulfilled_at = datetime.now()
+    change_email_request.request_fulfilled_at = datetime.now(timezone.utc)
     return HttpResponse(content="Successfully changed email", status=200)
 
 @method(["POST"])
@@ -315,10 +361,7 @@ def change_password_code_handler(request: HttpRequest):
     if change_password_request.is_fulfilled():
         return HttpResponse(content="Code already used", status=400)
     
-    if change_password_request.new_password != change_password_request.confirm_new_password:
-        return HttpResponse(content="New password must match confirm new password", status=400)
-    
     user.set_password(change_password_request.new_password)
     user.save()
-    change_password_request.request_fulfilled_at = datetime.now()
+    change_password_request.request_fulfilled_at = datetime.now(timezone.utc)
     return HttpResponse(content="Successfully changed password", status=200)
